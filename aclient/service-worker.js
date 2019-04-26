a232dfdc30d39e74130b4a38a23e9c24a31a9aeb
Space School <space.school.kg@gmail.com>
2019-04-26 15:52:12 +0600

    check

--- aclient/service-worker.js
+++ aclient/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c7fc09a1fc488186c55348970dd7389.js"
+  "/precache-manifest.c899faa9b4ae24085a7da0aad9023699.js"
 );
 
 workbox.clientsClaim();
