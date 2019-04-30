551bb434c7cfdf0cff8457e45934a6186aad8314
Space School <space.school.kg@gmail.com>
2019-04-30 14:45:16 +0600

    check

--- models/orderMissPolin.js
+++ models/orderMissPolin.js
@@ -10,7 +10,7 @@ const OrderMissPolinSchema = mongoose.Schema({
         required: true,
     },
     sum: {
-        type: Number,
+        type: String,
         required: true
     },
     kategory: {
